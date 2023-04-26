8a6c85bd567b7bccd4f7beca29f4cd780f1f91fb
kutsarvasya <vkutsar89@gmail.com>
2023-04-26 12:05:42 +0200

    fixed bugs

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-
-const galleryEl = document.querySelector('.gallery')
-
+const galleryEl = document.querySelector(".gallery");
 
 const createGallery = (pictures) => {
-    return pictures.map(({ preview, original, description }) => {
-      return  `<li class="gallery__item">
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
         <a class="gallery__link" href="large-image.jpg">
         <img
         class="gallery__image"
@@ -16,31 +15,26 @@ const createGallery = (pictures) => {
         alt=${description}
         />
         </a>
-        </li>`
-    }).join('') 
-}
+        </li>`;
+    })
+    .join("");
+};
 
-galleryEl.insertAdjacentHTML('afterbegin', createGallery(galleryItems))
+galleryEl.insertAdjacentHTML("afterbegin", createGallery(galleryItems));
 
-galleryEl.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (e.target === e.currentTarget) return
-    
-    const originalPicture = e.target.dataset.source
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.tagName !== "IMG") return;
 
-    
-    const instance = basicLightbox.create(`
+  const originalPicture = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
     <img src=${originalPicture}>
-`)
-    instance.show()
-    document.body.addEventListener('keyup', (e) => {
-    if (e.code === 'Escape') {
-    instance.close()
-} 
-})
-})
-
-
-
-
-
+`);
+  instance.show();
+  document.body.addEventListener("keyup", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,40 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery')
-
+const galleryEl = document.querySelector(".gallery");
 
 const createGallery = (pictures) => {
-    return pictures.map(({ preview, original, description }) => {
-      return  `<li class="gallery__item">
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
    <a class="gallery__link" href=${original}>
       <img class="gallery__image" src=${preview} alt=${description} />
    </a>
-</li>`
-    }).join('') 
-}
+</li>`;
+    })
+    .join("");
+};
 
-galleryEl.insertAdjacentHTML('afterbegin', createGallery(galleryItems))
+galleryEl.insertAdjacentHTML("afterbegin", createGallery(galleryItems));
 
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
-
-
-
-galleryEl.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (e.target === e.currentTarget) return
-
-    var lightbox = new SimpleLightbox('.gallery a',
-        {
-        captionsData: 'alt',
-        captionDelay: 250
-       });
-    lightbox.open(e.target)
- 
-
-})
-
-
-
-
-
+lightbox();
